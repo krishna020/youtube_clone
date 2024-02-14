0290d8e7f7762b78f8cbb2293d5f125263eaa189
krishna <krishnasahu0201@gmail.com>
2024-02-14 13:49:53 +0530

    using multer and cloudnary setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,29 @@
+import multer from 'multer';
+import uuid from 'uuid'
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const { originalname } = file
+  
+      //cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, `${originalname}${uuid.v4()}`)
+    }
+  })
+  const fileFilter = (req, file, cb) => {
+    // Check the MIME type for each file individually
+    const { mimetype } = file;
+  
+    // To reject this file, pass `false`
+    if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      cb(new multer.MulterError('LIMIT_UNEXPECTED_FILE'), false);
+    } else {
+      cb(null, true);
+    }
+  };
+  
+  const upload = multer({ storage, fileFilter, limits: { fileSize: 3 * 1024*1024} })
+  export default upload
---
+++ src/utils/cloudnary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from 'cloudinary'
+import { response } from 'express';
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_CLOUD_API_KEY, 
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET
+});
+
+const cloudnaryUploader= async (uploadFile)=>
+{
+  if(!uploadFile) return null;
+    try
+    {
+        await cloudinary.uploader.upload(uploadFile,{resource_type:"auto"})
+        console.log('file successfully uploaded..'+response.url)
+        return response
+    }
+    catch(err)
+    {
+      fs.unlinkSync(uploadFile)
+    }
+  
+
+}
+
+
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
